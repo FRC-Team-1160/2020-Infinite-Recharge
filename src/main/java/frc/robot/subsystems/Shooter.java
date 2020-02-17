33a00e1b15a83e063e48c1e1b16a376bdf9b7c78
jeff <jeff.zw07@gmail.com>
2020-02-17 14:59:53 -0800

    pid for shooter (not tested yet [must do])

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -7,10 +7,14 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANPIDController;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.PortConstants;
 
@@ -21,8 +25,15 @@ public class Shooter extends SubsystemBase {
   private static Shooter m_instance;
 
   private final CANSparkMax  m_leftShooter, m_rightShooter;
+  private CANEncoder         m_leftEncoder, m_rightEncoder;
 
-  private final DifferentialDrive m_mainShoot;
+  private CANPIDController speedController;
+
+  //private final DifferentialDrive m_mainShoot;
+
+  private double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput, velocity;
+
+  private boolean run;
 
   public static Shooter getInstance(){
     if (m_instance == null){
@@ -34,20 +45,86 @@ public class Shooter extends SubsystemBase {
   public Shooter() {
     m_leftShooter = new CANSparkMax(PortConstants.LEFT_SHOOTER, MotorType.kBrushless);
     m_rightShooter = new CANSparkMax(PortConstants.RIGHT_SHOOTER, MotorType.kBrushless);
-
     
     m_leftShooter.restoreFactoryDefaults();
     m_rightShooter.restoreFactoryDefaults();
 
-    m_mainShoot = new DifferentialDrive(m_leftShooter, m_rightShooter);
+    // inverse of left motor
+    m_rightShooter.follow(m_leftShooter, true);
+
+    m_leftEncoder = m_leftShooter.getEncoder();
+    m_rightEncoder = m_rightShooter.getEncoder();
+
+    // should be the same as right motor
+    speedController = m_leftShooter.getPIDController();
+
+    speedController.setFeedbackDevice(m_leftEncoder);
+
+    // PID coefficients
+    kP = 0.1; 
+    kI = 1e-4;
+    kD = 1; 
+    kIz = 0; 
+    kFF = 0; 
+    kMaxOutput = 1; 
+    kMinOutput = -1;
+
+    // set PID coefficients
+    speedController.setP(kP);
+    speedController.setI(kI);
+    speedController.setD(kD);
+    speedController.setIZone(kIz);
+    speedController.setFF(kFF);
+    speedController.setOutputRange(kMinOutput, kMaxOutput);
+
+    // display PID coefficients on SmartDashboard
+    SmartDashboard.putNumber("P Gain", kP);
+    SmartDashboard.putNumber("I Gain", kI);
+    SmartDashboard.putNumber("D Gain", kD);
+    SmartDashboard.putNumber("I Zone", kIz);
+    SmartDashboard.putNumber("Feed Forward", kFF);
+    SmartDashboard.putNumber("Max Output", kMaxOutput);
+    SmartDashboard.putNumber("Min Output", kMinOutput);
+    SmartDashboard.putNumber("Set Velocity", 0);
+
+    //m_mainShoot = new DifferentialDrive(m_leftShooter, m_rightShooter);
   }
 
-  public void shooterControl(double input){
-    m_mainShoot.tankDrive(input, input);
+  public void shooterControl(double velocity){
+    if (velocity > 0.0) speedController.setReference(velocity, ControlType.kVelocity);
+    //m_mainShoot.tankDrive(input, input);
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    // read PID coefficients from SmartDashboard
+    double p = SmartDashboard.getNumber("P Gain", 0);
+    double i = SmartDashboard.getNumber("I Gain", 0);
+    double d = SmartDashboard.getNumber("D Gain", 0);
+    double iz = SmartDashboard.getNumber("I Zone", 0);
+    double ff = SmartDashboard.getNumber("Feed Forward", 0);
+    double max = SmartDashboard.getNumber("Max Output", 0);
+    double min = SmartDashboard.getNumber("Min Output", 0);
+    velocity = SmartDashboard.getNumber("Set Velocity", 0);
+
+    // if PID coefficients on SmartDashboard have changed, write new values to controller
+    if((p != kP)) { speedController.setP(p); kP = p; }
+    if((i != kI)) { speedController.setI(i); kI = i; }
+    if((d != kD)) { speedController.setD(d); kD = d; }
+    if((iz != kIz)) { speedController.setIZone(iz); kIz = iz; }
+    if((ff != kFF)) { speedController.setFF(ff); kFF = ff; }
+    if((max != kMaxOutput) || (min != kMinOutput)) { 
+        speedController.setOutputRange(min, max); 
+        kMinOutput = min; kMaxOutput = max; 
+    }
+
+    // if (run)
+    // {
+    //   speedController.setReference(velocity, ControlType.kVelocity);
+    // }
+
+    SmartDashboard.putNumber("SetPoint", velocity);
+    SmartDashboard.putNumber("ProcessVariable", m_leftEncoder.getVelocity());
   }
 }
